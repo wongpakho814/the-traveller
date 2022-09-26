47a53481cd6c142d49ccc0ab3ce9f1a980370498
wongpakho814 <wongpakho814@gmail.com>
2022-09-26 15:43:48 +1000

    Implemented js code to render the 7 days weather forecast to the webpage
    Added new css to style.css, and changed some indentation in index.html

--- assets/js/weather-api.js
+++ assets/js/weather-api.js
@@ -3,6 +3,10 @@ var apiKey = "2f8fa537b9d9c6dee91715b6634d3f8e"; // The personal API Key
 // Handles the submit button, which fetches the city name that the user input and get the coordinates of the city
 function formSubmitHandler(event) {
     event.preventDefault();
+
+    $(".forecast-div").addClass("columns");
+    $("#weather-hint").hide();
+
     let city = $("#city-name").val();
   
     if (city) {
@@ -65,6 +69,61 @@ function getCityWeather(lat, lon, name) {
         .catch(function (error) {
             alert("Unable to connect to OpenWeather");
         });
+}
+
+// Displays the weather data fetched from the API
+function displayWeather(weather, city) {
+    // Clear the innerHTML of the previous results, if there are
+    document.querySelector(".forecast-div").innerHTML = "";
+
+    // Capitalize the first letter of each word in the city's name
+    city = capitalizeWords(city);
+
+    // Rendering the weather for today and the next 6 days
+    for (let i = 0; i < weather.daily.length - 1; i++) {
+        renderTodayWeather(i, weather, city);
+    }
+}
+
+// Creates and renders the HTML for today's weather
+function renderTodayWeather(i, weather, city) {
+    let months = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
+    let DT = new Date(weather.daily[i].dt * 1000); 
+
+    let temp = weather.daily[i].temp.day;
+    let precipitation = weather.daily[i].pop;
+    let humid = weather.daily[i].humidity;
+
+    let weatherEl = document.createElement("div");
+    weatherEl.setAttribute("class", "column mr-3 weather-div");
+    let titleEl = document.createElement("h2"); // e.g. Atlanta (DD/MM/YYYY)
+    titleEl.textContent = city + " (" + DT.getDate() + "/" + months[DT.getMonth()] + "/" + DT.getFullYear() + ") ";
+    let iconEl = document.createElement("img"); // e.g. 🔆
+    iconEl.setAttribute("src", "http://openweathermap.org/img/wn/" + weather.daily[i].weather[0].icon + "@2x.png");
+    let tempEl = document.createElement("p");
+    tempEl.textContent = "Temperature: " + temp + "°C";
+    let popEl = document.createElement("p");
+    popEl.textContent = "Precipitation: " + precipitation + "%";
+    let humidEl = document.createElement("p");
+    humidEl.textContent = "Humidity: " + humid + "%";
+
+    let forecastEl = document.querySelector(".forecast-div");
+    forecastEl.appendChild(weatherEl);
+    weatherEl.appendChild(titleEl);
+    weatherEl.appendChild(iconEl);
+    weatherEl.appendChild(tempEl);
+    weatherEl.appendChild(popEl);
+    weatherEl.appendChild(humidEl);
+}
+
+// Capitalize the first letter of each word
+function capitalizeWords(sentence) {
+    sentence = sentence.toLowerCase();
+    const words = sentence.split(" ");
+
+    return words.map((word) => { 
+        return word[0].toUpperCase() + word.substring(1); 
+    }).join(" ");
 }
 
 // Initialize the page by adding the event listener to the submit buttons
