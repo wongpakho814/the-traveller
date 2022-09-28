45baad3a7f021e7a582ac9775bb1cceac736c193
James Prince <james.prince1@gmail.com>
2022-09-29 09:33:36 +1000

    After identifying issues with the OpenRouteService and OpenPOIService API, we have had to switch API. I have tested several APIs and have settled on FourSquare

--- assets/js/points-of-interest.js
+++ assets/js/points-of-interest.js
@@ -1,52 +1,68 @@
 
-const orsAPI = '5b3ce3597851110001cf6248e6166b0f4e4d4741a8c6950dfe78e460';
+const owAPI = '2f8fa537b9d9c6dee91715b6634d3f8e';
+const fsAPI = 'fsq3gTR+vn6z9rNX3/C+A8iVWbIYIdcHkUajSPCoExvJ9Kc='
 
 
-async function getPOI (lat, lon){
 
-    let poiCollection = await searchForTouristPOI([lat,lon], orsAPI);
+async function getTouristPOIfsq (lat, lon){
+
+        let apiEndpoint = `https://api.foursquare.com/v3/places/search?ll=${lat},${lon}&radius=2000&limit=50&sort=RATING&categories=10000,16000`
+
+        let response = await fetch (apiEndpoint, {
+            'headers': {
+                'Accept': 'application/json',
+                'Authorization': fsAPI
+            }
+        })
+
+        if (response.status !== 200)
+        {
+            throw new Error (`Failed to invoke API`)
+        }
+
+        let data = await response.json();
+        return data;
+}
+
+
+
+async function getPlaceInformation (fsqID){
+    let apiEndpoint = `https://api.foursquare.com/v3/places/${fsqID}`
+
+    let response = await fetch (apiEndpoint, {
+        'headers': {
+            'Accept': 'application/json',
+            'Authorization': fsAPI
+        }
+    })
+
+    if (response.status !== 200)
+    {
+        throw new Error (`Failed to invoke API`)
+    }
+
+    let data = await response.json();
+    return data;
+}
+
+
+async function getPOIlist (lat, lon){
+
+    let data = await getTouristPOIfsq(lat, lon);
 
     let formattedCollection = [];
 
-    for(let i = 0; i < poiCollection.features.length; i++){
+    for(let i = 0; i < data.results.length; i++){
         
-        //Skip loop if features have no tags/name - We dont care about these because they are difficult to 
-        if(!poiCollection.features[i].properties.osm_tags || poiCollection.features[i].properties.osm_tags === undefined){
-            //console.log(i); 
-            continue;
-        }
-
-        //Returned values will always contain: Coordinates (as an array), a geojson point (this can be ignored safely and is there just incase we need it), an array of category objects, the name of the point of interest, the distance it is from the point provided in Meters
         let poi = {
-            id: poiCollection.features[i].properties.osm_id,
-            coordinates: poiCollection.features[i].geometry.coordinates,
-            geojson: poiCollection.features[i].geometry,
-            categories: [], // for in loop over categories to update soon
-            name: poiCollection.features[i].properties.osm_tags.name,
-            distance: poiCollection.features[i].properties.distance, //distance from central point
-        }
-
-
-        //if the data returned contains a website, a phone number, wheelchair access (either no, limited or yes), opening hours, add it to the object. Not all data returned will have this.
-        if(poiCollection.features[i].properties.osm_tags.website){
-            poi.website = poiCollection.features[i].properties.osm_tags.website;
-        }
-
-        if(poiCollection.features[i].properties.osm_tags.phone){
-            poi.phone = poiCollection.features[i].properties.osm_tags.phone;
-        }
-
-        if(poiCollection.features[i].properties.osm_tags.wheelchair){
-            poi.wheelchair = poiCollection.features[i].properties.osm_tags.wheelchair;
-        }
-
-        if (poiCollection.features[i].properties.osm_tags.opening_hours){
-            poi.opening_hours = poiCollection.features[i].properties.osm_tags.opening_hours;
-        }
-
-        //add the category objects information 
-        for (category in poiCollection.features[i].properties.category_ids){
-            poi.categories.push(poiCollection.features[i].properties.category_ids[category]);
+            id: data.results[i].fsq_id,
+            latitude: data.results[i].geocodes.main.latitude,
+            longitude: data.results[i].geocodes.main.longitude,
+            name: data.results[i].name,
+            distance: data.results[i].distance,
+            related_places: data.results[i].related_places.children,
+            address: data.results[i].location.formatted_address,
+            
         }
 
 
@@ -59,8 +75,14 @@ async function getPOI (lat, lon){
 
 // Test function. 
 // async function testFn (){
-//     let pois = await getPOI(151.209421, -33.868633); // melbourne
+//     let location = await geocodeLocation('Hong Kong', owAPI, 1);
+//     console.log(location);
+
+//     let pois = await getPOIlist(location[0].lat, location[0].lon); 
 //     console.log(pois);
 // }
 
 // testFn();
+
+
+
