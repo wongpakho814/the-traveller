dac4fcd82bbad03c6f15529415955d4ce1034fba
chilikilong112 <chiliki.long@gmail.com>
2022-09-28 17:01:37 +1000

    attraction list

--- assets/js/list-interactivity.js
+++ assets/js/list-interactivity.js
@@ -8,7 +8,7 @@ async function renderEvents() {
     for (let i = 0; i < 10; i++) {
         let eventULEl = document.querySelector(".first-list");
         let newEventEl = document.createElement("li");
-        newEventEl.setAttribute("class", "first-list-item");
+        newEventEl.setAttribute("class", "first-list-item box");
         newEventEl.setAttribute("data-id", data[i].id);
         newEventEl.textContent = data[i].name + " ";
         let addBtnEL = document.createElement("button");
@@ -22,8 +22,10 @@ async function renderEvents() {
 // Handles the "+" button in the event list
 async function addEventHandler(event) {
     let userListEl = document.querySelector(".user-list");
-    let copiedListItem = cloneItem(event.target.closest("li"));
+    let currentListItem = event.target.closest("li");
+    let copiedListItem = cloneItem(currentListItem, "remove-btn");
     userListEl.appendChild(copiedListItem);
+    currentListItem.remove();
 }
 
 // Handles the "-" button in the user list
@@ -35,17 +37,18 @@ function removeEventHandler(event) {
 }
 
 // Clone the list item to copy over to another list
-function cloneItem(item) {
+function cloneItem(item, btnClass) {
     let newListEl = document.createElement("li");
-    newListEl.setAttribute("class", "user-list-item");
+    newListEl.setAttribute("class", "user-list-item box");
     newListEl.textContent = item.textContent;
     // Remove the text of the button since textContent get the texts in the child elements as well
-    newListEl.textContent = newListEl.textContent.replace("+", "").slice(0, -1);
+    newListEl.textContent = newListEl.textContent.replace("➕", "").slice(0, -1);
     newListEl.setAttribute("data-id", newListEl.textContent);
     newListEl.textContent = newListEl.textContent + " ";
     // Create the "-" button in the user's list
     let newListBtnEl = document.createElement("button");
-    newListBtnEl.textContent = "-";
+    newListBtnEl.textContent = "➖";
+    newListBtnEl.setAttribute("class", btnClass);
     newListEl.appendChild(newListBtnEl);
     return newListEl;
 }
@@ -62,14 +65,19 @@ window.onclick = function(event) {
     }
 }
 $("#modal-confirm").click(function() {
+    let firstListEl = document.querySelector(".first-list");
     let listItem = document.querySelector("[data-id= '" + currListItemID + "']");
-    listItem.closest("ul").removeChild(listItem.closest("li"));
+    let currentListItem = listItem.closest("li");
+    let copiedListItem = cloneItem(currentListItem, "add-btn")
+    listItem.closest("ul").removeChild(currentListItem);
+    firstListEl.appendChild(copiedListItem);
     modal.style.display = "none";
-});
+}); 
 
 // Initiate the script by adding event listeners to the "+" and "-" buttons
 function init() {
-    $(".first-list").on("click", addEventHandler);
-    $(".user-list").on("click", removeEventHandler);
+    $(document).on("click", ".add-btn", addEventHandler);
+    $(document).on("click", ".remove-btn", removeEventHandler);
 }
+
 init();
